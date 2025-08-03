fcd889b65a4fa2cb48323f0bc85bc5799d48aa28
Vincent Reitz <vincent.reitz@gmail.com>
2025-08-03 19:22:30 +0200

    Missed that one

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: mode === "development" ? "/" : "/gridflow-organizer",
   plugins: [
     tanstackRouter({
       target: "react",
